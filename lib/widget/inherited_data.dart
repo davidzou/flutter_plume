8bda52f6e6a872ec42f702ae6d025fba71fd2cbb
zouzhangwei <wearecisco@gmail.com>
2020-08-12 17:12:36 +0800

    add inherited data abstract class

---
+++ lib/widget/inherited_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+///
+/// 登录数据共享。
+///
+/// ### Example:
+///
+///     class LoginPageInherited extends InheritedDataWidget<String> {
+///       static LoginPageInherited of(BuildContext context) {
+///         return context.dependOnInheritedWidgetOfExactType<LoginPageInherited>();
+///       }
+///     }
+///
+abstract class InheritedDataWidget<T> extends InheritedWidget {
+
+  InheritedDataWidget({Key key, this.data, this.child}) : super(key: key, child: child);
+  final Widget child;
+  final T data;
+
+  @override
+  bool updateShouldNotify(InheritedDataWidget oldWidget) {
+    return oldWidget.data != this.data;
+  }
+}
