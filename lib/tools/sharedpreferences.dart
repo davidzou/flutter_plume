f272996dd141435674c416e5b703b856ee46611f
zouzhangwei <wearecisco@gmail.com>
2020-09-09 09:59:17 +0800

    add sharedpreferences support and normal class

---
+++ lib/tools/sharedpreferences.dart
@@ -0,0 +1,63 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+///
+/// SharedPreferences 工具类
+///
+class RWSharedPreferences {
+  RWSharedPreferences._();
+
+  static saveBool(String key, bool value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setBool(key, value);
+  }
+
+  static saveInt(String key, int value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setInt(key, value);
+  }
+
+  static saveDouble(String key, double value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setDouble(key, value);
+  }
+
+
+  static saveString(String key, String value) async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setString(key, value);
+  }
+
+  static saveStringArray(String key, List<String> value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.setStringList(key, value);
+  }
+
+  // 获取SharedPreferences
+
+  static Future<bool> getBool(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key);
+  }
+
+
+  static Future<int> getInt(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key);
+  }
+
+  static Future<double> getDouble(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble(key);
+  }
+
+  static Future<String> getString(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  static Future<List<String>> getStringArray(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(key);
+  }
+
+}
