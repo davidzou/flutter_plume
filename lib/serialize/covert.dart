d2d4c22b6be9e03ad4183a56843c2d3b97ae0eec
zouzhangwei <wearecisco@gmail.com>
2020-11-23 16:47:21 +0800

    add new interface covert what to serialize somebody

---
+++ lib/serialize/covert.dart
@@ -0,0 +1,32 @@
+///
+/// [T] 被转换的数据对象。
+///
+mixin MapConvert<T> {
+  ///
+  /// 将对象转换为Map
+  ///
+  /// ```
+  ///   @override
+  ///   Map<String, dynamic> toMap() {
+  ///     var map = <String, dynamic>{
+  ///       "id": id,
+  ///       "name": name,
+  ///     };
+  ///     return map;
+  ///   }
+  /// ```
+  ///
+  Map<String, dynamic> toMap();
+
+  ///
+  /// 将Map转换为对象
+  ///
+  /// ```
+  ///   @override
+  ///   Model fromMap(Map<String, dynamic> map) {
+  ///     return Model(id: map["id"], name: map["name"]);
+  ///   }
+  /// ```
+  ///
+  T fromMap(Map<String, dynamic> map);
+}
