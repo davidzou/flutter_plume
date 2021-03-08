7c12f038f7ef21807b0a9635ef7ccbdb6ce716a8
zouzhangwei <wearecisco@gmail.com>
2021-03-08 10:30:51 +0800

    update calendar by DayClip

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -19,21 +19,38 @@ import 'package:plume/calendar/tools/date_math.dart';
 /// ```
 ///
 class Calendar {
+
   ///
   /// 计算显示区域。
   ///
   /// 月
   ///
-  List<Widget> getPage(BuildContext context, InnerCalendar innerCalendar, {int year, int month}) {
+  List<CalendarClip> getPage(BuildContext context, InnerCalendar innerCalendar,
+      {List<DayClipModel> data, int year, int month,}) {
     /// 要显示哪年？
-    int _year = year ?? DateTime.now().year;
+    int _year = year ?? DateTime
+        .now()
+        .year;
+
     /// 要显示哪个月？
-    int _month = month ?? DateTime.now().month;
+    int _month = month ?? DateTime
+        .now()
+        .month;
+
     /// 一共要有多少个Widget，即天
     int _days = DateMath().getDaysOfMonth(_month, year: _year);
     print("year:$_year   month:$_month");
     int offset = DateTime(_year, _month, 1).weekday;
-    return _getScope(context, innerCalendar, _year, _month, _days, offset);
+
+
+    return _getScope(
+        context,
+        innerCalendar,
+        data,
+        _year,
+        _month,
+        _days,
+        offset);
   }
 
   ///
@@ -42,7 +59,9 @@ class Calendar {
   /// * days    每个月的天数
   /// * offset   偏移量
   ///
-  List<Widget> _getScope(BuildContext context, InnerCalendar innerCalendar, int year, int month, int days, int offset) {
+  List<CalendarClip> _getScope(BuildContext context,
+      InnerCalendar innerCalendar, List<DayClipModel> data, int year, int month,
+      int days, int offset,) {
     print("days:$days  offset:$offset");
 
     //
@@ -53,7 +72,7 @@ class Calendar {
     int _tuning = 0;
     assert(_tuning == 0 || _tuning == 1);
 
-    List<String> _tuningTitle;
+    List<int> _tuningTitle;
     if (_tuning == 0) {
       _tuningTitle = BIBLE_WEEK_TITLE;
     } else {
@@ -61,30 +80,42 @@ class Calendar {
     }
     assert(_tuningTitle.length == 7);
 
-    List<Widget> children = <Widget>[];
+    // 每个DayClip的数据
+    // List<Widget> children = <Widget>[];
+    //
+    List<CalendarClip> dayClips = <CalendarClip>[];
+    // 如果开头就是7天的偏移，那么就不需要补充。
     bool spec = (offset - _tuning) % 7 != 0;
-
+    // 总量（包含了前面的补空，后面不算）
     int _num = days + (spec ? (offset - _tuning) : 0);
     for (int i = 0; i < _num; i++) {
       if (i < (offset - _tuning) && spec) {
         /// FIXME 前要保留上个月的结束，后要保留下个月的开始
-        /// 填空位
-        children.add(Text(""));
+        // 填空位
+        dayClips.add(DayClip(DayClipModel(year: year, month: month, day: -1),
+            innerCalendar: innerCalendar));
       } else {
+        // 每天
         if (!spec) {
-          children.add(Text("${(i + 1)}"));
+          DayClipModel _dayClipModel = data?.firstWhere((
+              DayClipModel element) =>
+          (element.year == year && element.month == month &&
+              element.day == i + 1));
+          print(_dayClipModel);
+          dayClips.add(DayClip(_dayClipModel, innerCalendar: innerCalendar));
         } else {
-          int day = (i + 1) - (offset - _tuning);
-          // children.add(Text("$day"));
-          children.add(
-              innerCalendar?.buildDayChild(context, year, month, day) ??
-                  Text("$day"));
+          int _day = (i + 1) - (offset - _tuning);
+          DayClipModel _dayClipModel = data.firstWhere((DayClipModel element) =>
+          (element.year == year && element.month == month && element.day == _day));
+
+          print(_dayClipModel);
+          dayClips.add(DayClip(_dayClipModel, innerCalendar: innerCalendar));
         }
       }
     }
     // 头部标题，标记星期几，具体展现方式见实现
-    children.insertAll(0, _tuningTitle.map((e) => Text(e)));
-    return children;
+    dayClips.insertAll(0, _tuningTitle.map((e) => WeekDayClip(e)));
+    return dayClips;
   }
 
 }
@@ -92,13 +123,68 @@ class Calendar {
 ///
 /// 周表
 ///
-class WeekDayClip {
+class WeekDayClip extends CalendarClip {
   WeekDayClip(this.dayOfWeek);
+
   final int dayOfWeek;
+
+  getData() => dayOfWeek;
+
+  Widget build() {
+    switch (dayOfWeek) {
+      case DateTime.monday:
+        return Text("一");
+      case DateTime.tuesday:
+        return Text("二");
+      case DateTime.wednesday:
+        return Text("三");
+      case DateTime.thursday:
+        return Text("四");
+      case DateTime.friday:
+        return Text("五");
+      case DateTime.saturday:
+        return Text("六");
+      case DateTime.sunday:
+        return Text("日", style: TextStyle(color: Colors.orange),);
+    }
+  }
 
   @override
   String toString() {
     return 'WeekDayClip{dayOfWeek: $dayOfWeek}';
+  }
+}
+
+///
+/// 日
+///
+class DayClip extends CalendarClip {
+  // DayClip(this._year, this.month, this.day, {this.innerCalendar});
+  DayClip(this.dayClipModel, {this.innerCalendar});
+
+  // final int _year;
+  // final int month;
+  // ///
+  // /// 每日，-1为空
+  // ///
+  // final int day;
+  final DayClipModel dayClipModel;
+  final InnerCalendar innerCalendar;
+
+  getData() {
+    return innerCalendar != null ? innerCalendar.getData(
+        dayClipModel.year, dayClipModel.month, dayClipModel.day) : null;
+  }
+
+  Widget build() {
+    return dayClipModel.day == -1 ? Text("") : (innerCalendar != null
+        ? innerCalendar.buildDayClip(dayClipModel)
+        : Text("${dayClipModel.day}"));
+  }
+
+  @override
+  String toString() {
+    return 'DayClip{$dayClipModel}';
   }
 
 }
@@ -106,17 +192,30 @@ class WeekDayClip {
 ///
 /// 日历碎片，每个日历所在的
 ///
-class DayClip {
-  DayClip(this.day);
+/// 作用：
+///   * 展示标题用
+///   * 展示日期用
+///
+abstract class CalendarClip {
+  getData();
 
+  Widget build();
+}
+
+class DayClipModel {
+  DayClipModel({this.data, this.year, this.month, this.day});
+
+  final dynamic data;
+
+  final int year;
+  final int month;
   final int day;
 
   @override
   String toString() {
-    return 'DayModel{day: $day}';
+    return 'DayClipModel{year: $year, month: $month, day: $day}';
   }
 }
-
 
 /// 可扩展日历设置。
 ///
@@ -145,30 +244,28 @@ class DayClip {
 /// }
 /// ```
 abstract class InnerCalendar {
+
+  /// 获取数据
+  external DayClipModel getData(int year, int month, int day);
+
   /// 构建每一个日期子类布局，其中用于显示日期，和点击效果？。
-  external Widget buildDayChild(
-      BuildContext context, int year, int month, int day);
+  @deprecated
+  external Widget buildDayChild(int year, int month, int day);
+
+  external Widget buildDayClip(DayClipModel dayClipModel);
 }
 
 /// 默认
 mixin InnerCalendarMiXin on InnerCalendar {
+
   @override
-  Widget buildDayChild(BuildContext context, int year, int month, int day) {
+  @deprecated
+  Widget buildDayChild(int year, int month, int day) {
     // 只是日期的显示。
     return Text("$day");
   }
-}
 
-// /// 自定义扩展
-// class ExCalendar implements InnerCalendarMiXin {
-//   @override
-//   Widget buildDayChild(BuildContext context, int year, int month, int day) {
-//     return InkWell(
-//       child: Text("$day"),
-//       onTap: () {
-//         // 找数据，并显示。数据本地化存储
-//         print(day);
-//       },
-//     );
-//   }
-// }
+  Widget buildDayClip(DayClipModel dayClipModel) {
+    return Text("${dayClipModel.day}");
+  }
+}
--- lib/calendar/constant/constant.dart
+++ lib/calendar/constant/constant.dart
@@ -1,5 +1,27 @@
 
+
+
+
+
 /// 圣经上的排序
-const List<String> BIBLE_WEEK_TITLE = <String>["日", "一", "二", "三", "四", "五", "六"];
+// const List<String> BIBLE_WEEK_TITLE = <String>["日", "一", "二", "三", "四", "五", "六"];
+const List<int> BIBLE_WEEK_TITLE = <int>[
+  DateTime.sunday,
+  DateTime.monday,
+  DateTime.tuesday,
+  DateTime.wednesday,
+  DateTime.thursday,
+  DateTime.friday,
+  DateTime.saturday,
+];
 /// 中国历代的排序
-const List<String> CHINA_WEEK_TITLE = <String>["一", "二", "三", "四", "五", "六", "日"];
+// const List<String> CHINA_WEEK_TITLE = <String>["一", "二", "三", "四", "五", "六", "日"];
+const List<int> CHINA_WEEK_TITLE = <int>[
+  DateTime.monday,
+  DateTime.tuesday,
+  DateTime.wednesday,
+  DateTime.thursday,
+  DateTime.friday,
+  DateTime.saturday,
+  DateTime.sunday,
+];
