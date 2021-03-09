1a16c5be04ce00fc12217efb57b969e5d99e5bf1
zouzhangwei <wearecisco@gmail.com>
2021-03-09 15:01:55 +0800

    add new file gradient

---
+++ lib/colors/gradient.dart
@@ -0,0 +1,10 @@
+
+// 渐变配色
+
+import 'dart:ui';
+
+const List<Color> gradeGrey = <Color>[Color(0xffbdc3c7), Color(0xff2c3e50)];
+
+const List<Color> citrusPeel = <Color>[Color(0xfffdc830), Color(0xfff37335)];
+
+const List<Color> piggyPink = <Color>[Color(0xffee9ca7), Color(0xffffdde1)];
