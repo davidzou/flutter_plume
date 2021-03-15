94fa4517674054a933385725eb13787751220c3d
zouzhangwei <wearecisco@gmail.com>
2021-03-15 17:05:39 +0800

    update gadient colors

--- lib/colors/gradient.dart
+++ lib/colors/gradient.dart
@@ -1,11 +1,50 @@
 import 'dart:ui';
 
+
+
 // 渐变配色
 
 const List<Color> GradeGrey = const <Color>[Color(0xffbdc3c7), Color(0xff2c3e50)];
 
-const List<Color> CitrusPeel = const <Color>[Color(0xfffdc830), Color(0xfff37335)];
-
 const List<Color> PiggyPink = const <Color>[Color(0xffee9ca7), Color(0xffffdde1)];
 
 const List<Color> CoolBlues = const <Color>[Color(0xff2193b0), Color(0xff6dd5ed)];
+
+const List<Color> MegaTron = const <Color>[Color(0xffc6ffdd), Color(0xfffbd786), Color(0xfff7797d)];
+
+const List<Color> MoonlitAsteroid = const <Color>[Color(0xff0f2027), Color(0xff203a43), Color(0xff2c5364)];
+
+const List<Color> JShine = const <Color>[Color(0xff12c2e9), Color(0xffc471ed), Color(0xfff64f59)];
+
+const List<Color> EveningSunshine = const <Color>[Color(0xffb92b27), Color(0xff1565c0)];
+
+const List<Color> DarkOcean = const <Color>[Color(0xff373b44), Color(0xff4286f4)];
+
+const List<Color> CoolSky = const <Color>[Color(0xff2980b9), Color(0xff6dd5fa), Color(0xffffffff)];
+
+const List<Color> Yoda = const <Color>[Color(0xffff0099), Color(0xff493240)];
+
+const List<Color> Memariani = const <Color>[Color(0xffaa4b6b), Color(0xff6b6b83), Color(0xff3b8d99)];
+
+const List<Color> Amin = const <Color>[Color(0xff8eade2), Color(0xff4a00e0)];
+
+const List<Color> Harvey = const <Color>[Color(0xff1f4037), Color(0xff99f2c8)];
+
+const List<Color> Neuromancer = const <Color>[Color(0xfff953c6), Color(0xffb91d73)];
+
+const List<Color> AzurLane = const <Color>[Color(0xff7f7fd5), Color(0xff86a8e7), Color(0xff91eae4)];
+
+const List<Color> WitchingHour = const <Color>[Color(0xffc31432), Color(0xff240b6)];
+
+const List<Color> Flare = const <Color>[Color(0xfff12711), Color(0xfff5af19)];
+
+const List<Color> Metapolis = const <Color>[Color(0xff659999), Color(0xfff4791f)];
+
+const List<Color> KyooPal = const <Color>[Color(0xffdd3e54), Color(0xff6be585)];
+
+const List<Color> KyeMeh = const <Color>[Color(0xff8360c3), Color(0xff2ebf91)];
+
+const List<Color> KyooTah = const <Color>[Color(0xff544a7d), Color(0xffffd452)];
+
+
+const List<Color> CitrusPeel = const <Color>[Color(0xfffdc830), Color(0xfff37335)];
