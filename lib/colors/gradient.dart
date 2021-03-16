5af360c920f9b0971474edb0d15757c02a0afd35
zouzhangwei <wearecisco@gmail.com>
2021-03-16 10:17:18 +0800

    add new gradient color

--- lib/colors/gradient.dart
+++ lib/colors/gradient.dart
@@ -46,5 +46,68 @@ const List<Color> KyeMeh = const <Color>[Color(0xff8360c3), Color(0xff2ebf91)];
 
 const List<Color> KyooTah = const <Color>[Color(0xff544a7d), Color(0xffffd452)];
 
+const List<Color> ByDesign = const <Color>[Color(0xff009fff), Color(0xffec2f4b)];
+
+const List<Color> UltraVoilet = const <Color>[Color(0xff654ea3), Color(0xffeaafc8)];
+
+const List<Color> BurningOrange = const <Color>[Color(0xffff416c), Color(0xffff4b2b)];
+
+const List<Color> Wiretap = const <Color>[Color(0xff8a2387), Color(0xffe94057), Color(0xfff27121)];
+
+const List<Color> SummerDog = const <Color>[Color(0xffa8ff78), Color(0xff78ffd6)];
+
+const List<Color> Rastafari = const <Color>[Color(0xff1e9600), Color(0xfffff200), Color(0xffff0000)];
+
+const List<Color> SinCityRed = const <Color>[Color(0xffed213a), Color(0xff93291e)];
 
 const List<Color> CitrusPeel = const <Color>[Color(0xfffdc830), Color(0xfff37335)];
+
+const List<Color> BlueRaspberry = const <Color>[Color(0xff00b4db), Color(0xff0083b0)];
+
+const List<Color> Margo = const <Color>[Color(0xffffefba), Color(0xffffffff)];
+
+const List<Color> Magic = const <Color>[Color(0xff59c173), Color(0xffa17fe0), Color(0xff5d26c1)];
+
+const List<Color> EveningNight = const <Color>[Color(0xff005aa7), Color(0xfffffde4)];
+
+const List<Color> Vanusa = const <Color>[Color(0xffda4453), Color(0xff89216b)];
+
+const List<Color> Shifty = const <Color>[Color(0xff636363), Color(0xffa2ab58)];
+
+const List<Color> eXpresso = const <Color>[Color(0xffad5389), Color(0xff3c1053)];
+
+const List<Color> SlightOceanView = const <Color>[Color(0xffa8c0ff), Color(0xff3f2b96)];
+
+const List<Color> PureLust = const <Color>[Color(0xff333333), Color(0xffdd1818)];
+
+const List<Color> MoonPurple = const <Color>[Color(0xff4e54c8), Color(0xff8f94fb)];
+
+const List<Color> RedSunset = const <Color>[Color(0xff355c7d), Color(0xff6c5b7b), Color(0xffc06c84)];
+
+const List<Color> Shifter = const <Color>[Color(0xffbc4e9c), Color(0xfff80759)];
+
+const List<Color> WeddingDayBlues = const <Color>[Color(0xff40e0d0), Color(0xffff8c00), Color(0xffff0080)];
+
+const List<Color> SandToBlue = const <Color>[Color(0xff3e5151), Color(0xffdecba4)];
+
+const List<Color> Quepal = const <Color>[Color(0xff11998e), Color(0xff38ef7d)];
+
+const List<Color> PunYeta = const <Color>[Color(0xff108dc7), Color(0xffef8e38)];
+
+const List<Color> SublimeLight = const <Color>[Color(0xfffc5c7d), Color(0xff6a82fb)];
+
+const List<Color> Bighead = const <Color>[Color(0xffc94b4b), Color(0xff4b134f)];
+
+const List<Color> TaranTado = const <Color>[Color(0xff23074d), Color(0xffcc5333)];
+
+const List<Color> RelaxingRed = const <Color>[Color(0xfffffbd5), Color(0xffb20a2c)];
+
+const List<Color> Lawrencium = const <Color>[Color(0xff0f0c29), Color(0xff302b63), Color(0xff24243e)];
+
+const List<Color> Ohhappiness = const <Color>[Color(0xff00b09b), Color(0xff96c93d)];
+
+const List<Color> Delicate = const <Color>[Color(0xffd3cce3), Color(0xffe9e4f0)];
+
+const List<Color> Selenium = const <Color>[Color(0xff3c3b3f), Color(0xff605c3c)];
+
+const List<Color> Sulphur = const <Color>[Color(0xffcac531), Color(0xfff3f9a7)];
