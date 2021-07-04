7a656a0834e1bb98b5d3feca99325aaf1faee1ab
zouzhangwei <wearecisco@gmail.com>
2021-07-04 22:35:14 +0800

    UPDATE: update system_tools

--- lib/tools/system_tools.dart
+++ lib/tools/system_tools.dart
@@ -2,8 +2,12 @@ import 'package:flutter/services.dart';
 
 class SystemTools {
 
+  ///
   /// 直接退出程序
-  static Future<void> pop({bool? animated}) async {
+  ///
+  /// <a href="https://api.flutter.dev/flutter/services/SystemNavigator/pop.html">参考文档</a>
+  ///
+  static Future<void> exit({bool? animated}) async {
     await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop', animated);
   }
 }
