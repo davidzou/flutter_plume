68a4d5287e34565a0547ada92a085df0a5817ba8
zouzhangwei <wearecisco@gmail.com>
2021-10-18 15:06:44 +0800

    UPDATE: add new type dialog -- notice

--- example/lib/pages/page_dialogs.dart
+++ example/lib/pages/page_dialogs.dart
@@ -10,6 +10,7 @@ class DialogsPage extends StatelessWidget {
       body: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextButton.icon(
             onPressed: () {
@@ -28,6 +29,15 @@ class DialogsPage extends StatelessWidget {
                 )
               ],
             ),
+          ),
+          TextButton.icon(
+            onPressed: () {
+              DialogProvider.notice(context, title: "信息", content: "北朝鮮では重罪とされる金（ゴールド）の密輸。昨年、両江道（リャンガンド）の恵山（ヘサン）では、58キロもの金の密輸事件が摘発され、密輸業者、国境警備隊の副小隊長、中隊の保衛指導員ら7人が逮捕された。"
+                  "ドラマ『推しの王子様』（フジテレビ系／毎週木曜22時）の公式インスタグラムが21日、主演を務める比嘉愛未と渡邊圭祐の2ショットを公開。第1話終盤で二人が出会う「始まりのシーン」のオフショットが、まるでお笑いコンビのようだと反響を呼んでいる。",
+              indent: 28, dark: false,);
+            },
+            icon: Icon(Icons.call),
+            label: Text("自定义弹窗"),
           ),
         ],
       ),
--- lib/tools/dialog.dart
+++ lib/tools/dialog.dart
@@ -1,8 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DialogProvider {
-
   ///
   /// 显示一个对话框，只有一个按钮，取消则按空白区域即可。减少选择的困惑。
   ///
@@ -15,7 +16,12 @@ class DialogProvider {
   /// > message
   ///   内容，消息。
   ///
-  static Future<bool?> show(BuildContext context, {required String title, required String message, }) {
+  static Future<bool?> show(
+    BuildContext context, {
+    required String title,
+    required String message,
+    VoidCallback? onPressed,
+  }) {
     return showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -32,7 +38,7 @@ class DialogProvider {
                   width: 12,
                 ),
                 Icon(
-                  Icons.warning,
+                  Icons.warning_amber_rounded,
                   color: Colors.yellowAccent,
                 ),
               ],
@@ -44,13 +50,108 @@ class DialogProvider {
             actions: [
               TextButton(
                 onPressed: () {
+                  onPressed?.call();
                   Navigator.pop(context, true);
                 },
-                child: Text("没问题，一鼓作气！"),
+                child: Text("知道了"),
               ),
             ],
           );
         });
   }
 
+  ///
+  /// 通知，公告。
+  ///
+  /// ### 用途
+  /// * 提示信息
+  /// * 展示内容
+  /// * 展示说明
+  ///
+  /// ### 参数
+  /// * context     上下文<Must>
+  /// * title       信息标题<Must>
+  /// * content     信息内容<Must>
+  /// * buttonName  按钮文字
+  /// * onPressed   按钮点击事件响应
+  /// *
+  ///
+  static Future notice(
+    BuildContext context, {
+    required String title,
+    required String content,
+    String? buttonName,
+    VoidCallback? onPressed,
+    double indent = 28,
+    bool? dark,
+  }) {
+    // 取宽，屏幕的60%用于对话框。
+    double _width = MediaQuery.of(context).size.width * 72 / 100;
+    bool _dark = dark ?? (Theme.of(context).brightness == Brightness.dark);
+    final GlobalKey _dialogGlobalKey = GlobalKey();
+    return showDialog(
+      context: context,
+      barrierColor: _dark ? Color(0xaa000000) : Color(0x88B2EbF2),
+      builder: (BuildContext context) {
+        return Dialog(
+          key: _dialogGlobalKey,
+          backgroundColor: _dark ? Colors.black45 : Colors.white24,
+          // 对话框区域背景色
+          elevation: 12.0,
+          insetPadding: EdgeInsets.zero,
+          clipBehavior: Clip.hardEdge,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: _dialogGlobalKey.currentContext?.size?.width ?? _width,
+                padding: EdgeInsets.fromLTRB(indent, 13.0, 8.0, 8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: _dark ? Colors.white : Colors.black87),
+                ),
+              ),
+              SizedBox(
+                width: _dialogGlobalKey.currentContext?.size?.width ?? _width,
+                child: Divider(
+                  height: 8,
+                  indent: indent,
+                  color: _dark ? Colors.white : Colors.black87,
+                ),
+              ),
+              Container(
+                width: _dialogGlobalKey.currentContext?.size?.width ?? _width,
+                padding: EdgeInsets.fromLTRB((indent + 8.0), 8.0, 8.0, 8.0),
+                child: Text(
+                  content,
+                  style: TextStyle(color: _dark ? Colors.white : Colors.black87),
+                ),
+              ),
+              Container(
+                width: _dialogGlobalKey.currentContext?.size?.width ?? _width,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.only(right: 8.0),
+                child: TextButton(
+                  child: Text(
+                    buttonName ?? "Got it",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  onPressed: onPressed ?? () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
